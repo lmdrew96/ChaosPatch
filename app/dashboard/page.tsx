9846d30458698d584ea2a97f5a8a82924f37116b
lmdrew96 <lmdrew@udel.edu>
2026-04-05 16:09:04 -0400

    fix: HomeContent for /dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,8 @@
 import { auth } from '@clerk/nextjs/server';
 import { redirect } from 'next/navigation';
 import { NewProjectButton } from '@/app/new-project-button';
-import DashboardContent from '@/app/dashboard/dashboard-content';
+import { HomeContent } from '@/app/home-content';
+import { getAllPatches, getProjects } from '@/lib/queries';
 
 export default async function DashboardPage() {
   const { userId } = await auth();
@@ -9,12 +10,19 @@ export default async function DashboardPage() {
     redirect('/sign-in');
   }
 
+  const [projects, patches] = await Promise.all([
+    getProjects(userId),
+    getAllPatches(userId),
+  ]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         <NewProjectButton />
       </div>
-      <DashboardContent />
+      <div className="w-full max-w-5xl">
+        <HomeContent projects={projects} patches={patches} />
+      </div>
     </main>
   );
 }
